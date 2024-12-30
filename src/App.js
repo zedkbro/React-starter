ebb250d413b31e7a6595e2a51851394be251e6a3
zinabuk <kalayuzinabu@gmail.com>
2024-12-30 21:04:22 +0300

    api request from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, createContext, useContext, useRef } from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./Login";
 
@@ -8,44 +8,29 @@ import styles from "./my-style.module.css";
 const isAuth = true;
 
 function App() {
-  const cars = ["Tesla", "Nexus", "Bugatti"];
-  const [form, setForm] = useState({});
-  const [input, setInput] = useState("");
-  const inputElement = useRef("");
-
-  function handleForm(event) {
-    event.preventDefault();
-    alert(`Username: ${form.username}`);
-  }
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  }
-  let [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    inputElement.current = input;
-  }, [input]); // <- add the input variable here
-
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <h1>{count}</h1>
-          {/* <p style={{ color: "red", "font-size": "48px" }}> {cnt.current}</p> */}
-          <button onClick={() => setCount((c) => c + 1)}>+</button>
-          <p>{calculation}</p>
           <nav className={styles.bigred}>
-            <ul style={{ display: "flex", justifyContent: "center" }}>
-              <li>
-                <Link to="/signup" className={styles.bgred}>
-                  Signup
-                </Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
+            <ul
+              style={{
+                display: "flex flex-col",
+                justifyContent: "center",
+                gap: "24px",
+              }}
+            >
+              {data &&
+                data.slice(0, 10).map((item, i) => {
+                  return <li key={i}>{item.title}</li>;
+                })}
             </ul>
           </nav>
         </header>
@@ -54,39 +39,6 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-
-        <div>
-          <h2>Form Example</h2>
-          <p>{input}</p>
-          <p>{inputElement.current}</p>
-          <form className="login-form" onSubmit={handleForm}>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <input
-              type="text"
-              name="username"
-              value={form.username || ""}
-              onChange={handleChange}
-            />
-
-            <button type="submit" id="submit">
-              Submit
-            </button>
-          </form>
-        </div>
-
-        <div>
-          <h2>Cars List</h2>
-          <ul>
-            {cars.map((car, index) => (
-              <li key={index}>{car}</li>
-            ))}
-          </ul>
-        </div>
 
         <h1>{isAuth ? "Welcome Back!" : "Please Login"}</h1>
       </div>
